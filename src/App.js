201acdb65678678e472cf1c0279b0b8413b48fdb
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2022-07-01 01:18:26 +0800

    added class

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Component } from "react";
 import "./App.css";
+import Child from "./child";
 import Header from "./components/Header";
-class App extends Component() {
+class App extends Component {
   sayHello(name) {
     return `hello ${name}`;
   }
@@ -9,6 +10,7 @@ class App extends Component() {
     return (
       <div className="app">
         <Header />
+        <Child />
         <h1>{this.sayHello("sunny")}</h1>
       </div>
     );
