c947a98b68b9825e5f24d525fbd4b1303862fc84
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2022-07-01 01:25:49 +0800

    added state

--- src/Parent.js
+++ src/Parent.js
@@ -1,6 +1,14 @@
 import { Component } from "react";
 import Child from "./child";
 class Parent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      length: 10,
+      name: "Raj",
+      data: { id: "234", list: [11, 22, 33, 44] },
+    };
+  }
   render() {
     return (
       <div className="parent">
