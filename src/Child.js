1a242908a76235d698dbfca7b1a6801802304931
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2022-07-02 00:47:05 +0800

    added button to the child #1

--- src/Child.js
+++ src/Child.js
@@ -4,12 +4,14 @@ class Child extends Component {
   constructor(props) {
     super(props);
     this.state = {...this.props};
+    this.props.updateParentCountState('12');
   }
   render() {
     return (
       <div className="app">
         <h1>{this.state.userDetails.name} this is testing </h1>
         <h1>{this.state.newName} this is testing </h1>
+        <button>{this.state.newName} this is testing </button>
       </div>
     );
   }
