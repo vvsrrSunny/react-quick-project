1e319df89627378fdb4e5879e54b300ff3d79ca0
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2022-07-03 23:14:53 +0800

    fancy update done #1

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,10 @@ class Button extends Component {
   }
   render() {
     return (
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+      <button
+        onClick={() => this.props.onClick()}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+      >
         Button
       </button>
     );
