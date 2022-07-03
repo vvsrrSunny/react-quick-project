e4d97c2e936430f2ca1b06347e37ac14e0aee01e
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2022-07-03 14:32:12 +0800

    added object #1

--- src/Parent.js
+++ src/Parent.js
@@ -5,19 +5,23 @@ class Parent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dateOfBirth: "01/01/2000",
-      name: "Karthi Gollakota",
-      height: "5.10",
-      weight: 78,
-      data: {
-        id: "1",
-        tasks: [
-          "wake up in the morning",
-          "exercise",
-          "work hard",
-          { performancePercentage: 100 },
-        ],
-      },
+      data: [
+        {
+          dateOfBirth: "01/01/2000",
+          name: "Karthi Gollakota",
+          height: "5.10",
+          weight: 78,
+          data: {
+            id: "1",
+            tasks: [
+              "wake up in the morning",
+              "exercise",
+              "work hard",
+              { performancePercentage: 100 },
+            ],
+          },
+        },
+      ],
       childCallsCount: 0,
     };
   }
