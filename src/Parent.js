09752f886f3f6af2030d51fdd83bc99d4bc2fef3
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2022-07-03 14:18:09 +0800

    updated the data #1

--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,19 @@ class Parent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dateOfBirth: '01/01/2000',
+      dateOfBirth: "01/01/2000",
       name: "Karthi Gollakota",
       height: "5.10",
       weight: 78,
-      data: { id: "1", data: [11, 22, 33, 44] },
+      data: {
+        id: "1",
+        tasks: [
+          "wake up in the morning",
+          "exercise",
+          "work hard",
+          { performance: 100 },
+        ],
+      },
       childCallsCount: 0,
     };
   }
