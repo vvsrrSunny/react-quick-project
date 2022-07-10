2e94dc84326d185b2b104e9ebf9de36c6be6b03e
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2022-07-10 21:35:41 +0800

    added code #2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,14 @@ class App extends Component {
     return (
       <div className="app">
         <AppLayout> 
-          
+        <div className="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
+              {/* Replace with your content */}
+              <div className="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
+                <div className="border-4 border-white rounded-lg h-96" />
+
+              </div>
+              {/* /End replace */}
+            </div>
         </AppLayout>
       </div>
     );
--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -219,14 +219,7 @@ class UsersPage extends Component {
           </div>
 
           <main className="-mt-32">
-            <div className="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
-              {/* Replace with your content */}
-              <div className="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
-                <div className="border-4 border-white rounded-lg h-96" />
-
-              </div>
-              {/* /End replace */}
-            </div>
+          {this.props.children}
           </main>
         </div>
       </>
