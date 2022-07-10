871cbf48fa80ccaacf2ece64d343e70a86ce4eaa
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2022-07-10 20:01:48 +0800

    added code #2

--- src/components/UsersPage.jsx
+++ src/components/UsersPage.jsx
@@ -4,10 +4,10 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
 const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
+  name: 'Sunny',
+  email: 'Sunny@example.com',
   imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    'https://www.gstatic.com/webp/gallery/1.jpg',
 }
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
