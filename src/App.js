c49bc05a74af6b9a621e6c63295c271978a26142
Sunny Varanasi <Sunny.Varanasi@pentanet.com.au>
2022-07-12 16:48:47 +0800

    added a people state #2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,56 @@ import "./App.css";
 import AppLayout from "./components/AppLayout";
 import UsersTable from "./components/UsersTable";
 class App extends Component {
-  sayHello(name) {
-    return `hello ${name}`;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: [
+        {
+          name: 'Matt',
+          title: 'Team Lead',
+          email: 'Matt@example.com',
+          role: 'Member',
+        },
+        {
+          name: 'Joush',
+          title: 'Senior Front-end Developer',
+          email: 'Joush@example.com',
+          role: 'Member',
+        },
+        {
+          name: 'Sunny Varanasi',
+          title: 'Software Developer',
+          email: 'sunny.varanasi@example.com',
+          role: 'Member',
+        },
+        {
+          name: 'sam',
+          title: 'Software Developer',
+          email: 'Sam@example.com',
+          role: 'Member',
+        },
+        {
+          name: 'Alex',
+          title: 'Software Developer',
+          email: 'Alex@example.com',
+          role: 'Member',
+        },
+        {
+          name: 'Ben',
+          title: 'Senior Software Developer',
+          email: 'Ben@example.com',
+          role: 'Member',
+        },
+      ],
+    };
   }
+
   render() {
     return (
       <div className="app">
         <AppLayout>
-            <UsersTable></UsersTable>
+          <UsersTable></UsersTable>
         </AppLayout>
       </div>
     );
