bd1c15774db7a142153e00b8d46c452af883336c
Sunny Varanasi <Sunny.Varanasi@pentanet.com.au>
2022-07-12 17:11:50 +0800

    added style #2

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -220,9 +220,11 @@ class UsersPage extends Component {
 
           <main className="-mt-32">
             <div className="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
-              {this.props.children}
+
               <div className="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
-                <div className="border-4 border-white rounded-lg h-96" />
+                <div className="border-4 border-white rounded-lg" >
+                  {this.props.children}
+                </div>
               </div>
             </div>
           </main>
