b2c86262f11e546f83d597886eab222fa4e5683d
Sunny Varanasi <Sunny.Varanasi@pentanet.com.au>
2022-07-19 00:17:15 +0800

    achived sloted vue in react #2

--- src/components/TableLayout.jsx
+++ src/components/TableLayout.jsx
@@ -3,19 +3,25 @@ import { Component } from "react";
 
 function Header() {
     return null
-  }
-export default class TableLayout extends Component{
+}
+
+function Body() {
+    return null
+}
+export default class TableLayout extends Component {
     static Header = Header;
+    static Body = Body;
 
     render() {
         const { children } = this.props;
-        console.log(children);
+        const header = children.find(el => el.type === Header);
+
         return (<div className="flex flex-col">
             <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                     <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
                         <table className="min-w-full divide-y divide-gray-300">
-                            {this.props.children}
+                                {header ? header.props.children : null}
                         </table>
                     </div>
                 </div>
