f8d17e50ccc93dc63cb24584b692b4c62d96fe34
Sunny Varanasi <Sunny.Varanasi@pentanet.com.au>
2022-07-19 00:29:14 +0800

    added named slots feature in react #2

--- src/components/TableLayout.jsx
+++ src/components/TableLayout.jsx
@@ -22,7 +22,14 @@ export default class TableLayout extends Component {
                 <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                     <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
                         <table className="min-w-full divide-y divide-gray-300">
-                                {header ? header.props.children : null}
+                            <thead className="bg-gray-50">
+                                <tr>
+                                    {header ? header.props.children : null}
+                                </tr>
+                            </thead>
+                            <tbody className="divide-y divide-gray-200 bg-white">
+                                {body ? body.props.children : null}
+                            </tbody>
                         </table>
                     </div>
                 </div>
--- src/components/UsersTable.jsx
+++ src/components/UsersTable.jsx
@@ -9,34 +9,28 @@ export default class UserTable extends Component {
       <div>
         <TableLayout>
           <TableLayout.Header>
-            <thead className="bg-gray-50">
-              <tr>
-                <TableHeaderCell>Name</TableHeaderCell>
-                <TableHeaderCell>Title</TableHeaderCell>
-                <TableHeaderCell>Email</TableHeaderCell>
-                <TableHeaderCell>Role</TableHeaderCell>
-                <th scope="col" className="relative py-3 pl-3 pr-4 sm:pr-6">
-                  <span className="sr-only">Edit</span>
-                </th>
-              </tr>
-            </thead>
-            <tbody className="divide-y divide-gray-200 bg-white">
-              {this.props.people.map((person) => (
-                <tr key={person.email}>
-                  <TableCell fontSize="font-medium" paddingXAxis="pl-4 pr-3 sm:pl-6" textColor="text-gray-900">{person.name}</TableCell>
-                  <TableCell>{person.title}</TableCell>
-                  <TableCell>{person.email}</TableCell>
-                  <TableCell>{person.role}</TableCell>
-                  <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                    <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                      Edit<span className="sr-only">, {person.name}</span>
-                    </a>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
+            <TableHeaderCell>Name</TableHeaderCell>
+            <TableHeaderCell>Title</TableHeaderCell>
+            <TableHeaderCell>Email</TableHeaderCell>
+            <TableHeaderCell>Role</TableHeaderCell>
+            <th scope="col" className="relative py-3 pl-3 pr-4 sm:pr-6">
+              <span className="sr-only">Edit</span>
+            </th>
           </TableLayout.Header>
           <TableLayout.Body>
+            {this.props.people.map((person) => (
+              <tr key={person.email}>
+                <TableCell fontSize="font-medium" paddingXAxis="pl-4 pr-3 sm:pl-6" textColor="text-gray-900">{person.name}</TableCell>
+                <TableCell>{person.title}</TableCell>
+                <TableCell>{person.email}</TableCell>
+                <TableCell>{person.role}</TableCell>
+                <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                  <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                    Edit<span className="sr-only">, {person.name}</span>
+                  </a>
+                </td>
+              </tr>
+            ))}
           </TableLayout.Body>
         </TableLayout>
       </div>
