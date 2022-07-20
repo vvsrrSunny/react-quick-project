99869e05f0f7394008ff726a47465751814a8a0c
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-07-21 01:02:23 +0800

    added edit button to a new component #3

---
+++ src/components/TableCellEdit.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export default class TableCellEdit extends Component {
+    render() {
+        return (
+            <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                    Edit<span className="sr-only">, {this.props.person.name}</span>
+                </a>
+            </td>
+        )
+    }
+}
--- src/components/UsersTable.jsx
+++ src/components/UsersTable.jsx
@@ -3,6 +3,7 @@ import TableCell from "./TableCell";
 import TableHeaderCell from "./TableHeaderCell";
 import TableLayout from "./TableLayout";
 import SliderLayout from "./SliderLayout";
+import TableCellEdit from "./TableCellEdit";
 
 export default class UserTable extends Component {
   render() {
@@ -25,11 +26,7 @@ export default class UserTable extends Component {
                 <TableCell>{person.title}</TableCell>
                 <TableCell>{person.email}</TableCell>
                 <TableCell>{person.role}</TableCell>
-                <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                  <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                    Edit<span className="sr-only">, {person.name}</span>
-                  </a>
-                </td>
+                <TableCellEdit person={person}></TableCellEdit>
               </tr>
             ))}
           </TableLayout.Body>
