5bad5af70a573f78c880b1b8becf5c91da578000
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-07-23 01:26:26 +0800

    multiple refs test #5

--- src/components/TableSlider.jsx
+++ src/components/TableSlider.jsx
@@ -4,13 +4,18 @@ import SliderLayout from "./SliderLayout";
 class TableSlider extends Component {
     render() {
         return (
-            <SliderLayout ref={this.props.sliderLayoutRef}>
-                
-            </SliderLayout>
+            <>
+                <SliderLayout ref={this.props.sliderLayoutRef}>
+
+                </SliderLayout>
+                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                    Button 2
+                </button>
+            </>
         )
     }
 }
 
 export default React.forwardRef((props, sliderLayoutRef) => <TableSlider
-sliderLayoutRef={sliderLayoutRef} {...props}
+    sliderLayoutRef={sliderLayoutRef} {...props}
 />);
