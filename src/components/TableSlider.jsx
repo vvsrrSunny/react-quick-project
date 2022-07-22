2f8a47d74d912908d6582ca423d5806a9b7054f3
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-07-23 00:33:18 +0800

    updating the table silder #4

--- src/components/TableSlider.jsx
+++ src/components/TableSlider.jsx
@@ -1,9 +1,10 @@
 import { Component } from "react";
+import TableLayout from "./TableLayout";
 
 export default class TableSlider extends Component {
     render() {
         return (
-<></>
+            <TableLayout ref={this.props.ref}></TableLayout>
         )
     }
 }
