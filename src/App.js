a8b8f25dc920dad3d7a6260bc2312c3297475c26
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-07-23 22:10:15 +0800

    added ids to the array #9

--- src/App.js
+++ src/App.js
@@ -9,36 +9,42 @@ class App extends Component {
     this.state = {
       people: [
         {
+          id: 1,
           name: 'Matt',
           title: 'Team Lead',
           email: 'Matt@example.com',
           role: 'Member',
         },
         {
+          id: 2,
           name: 'Josh',
           title: 'Senior Front-end Developer',
           email: 'Joush@example.com',
           role: 'Member',
         },
         {
+          id: 3,
           name: 'Sunny',
           title: 'Software Developer',
           email: 'sunny@example.com',
           role: 'Member',
         },
         {
+          id: 4,
           name: 'Sam',
           title: 'Software Developer',
           email: 'Sam@example.com',
           role: 'Member',
         },
         {
+          id: 5,
           name: 'Alex',
           title: 'Software Developer',
           email: 'Alex@example.com',
           role: 'Member',
         },
         {
+          id: 6,
           name: 'Ben',
           title: 'Senior Software Developer',
           email: 'Ben@example.com',
