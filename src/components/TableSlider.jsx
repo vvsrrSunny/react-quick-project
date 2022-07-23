26d5df47ff7f6072c089c8bed5a65445c6ff88f5
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-07-24 00:25:39 +0800

    updating to classes #9

--- src/components/TableSlider.jsx
+++ src/components/TableSlider.jsx
@@ -1,13 +1,5 @@
-import { Component } from "react";
 import React from "react"
 import SliderLayout from "./SliderLayout";
-function TableSlider(props) {
-        return (
-            <SliderLayout ref={props.sliderLayoutRef}>
-                
-            </SliderLayout>
-        )
-}
 
 export default TableSlider = React.forwardRef((props, sliderLayoutRef) => { return (
     <SliderLayout ref={sliderLayoutRef}>
