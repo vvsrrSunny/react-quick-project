d3bfae5681af41d918b3c7068fec61d002ddb79f
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-07-25 15:12:39 +0800

    added code

--- src/components/SliderLayout.jsx
+++ src/components/SliderLayout.jsx
@@ -56,23 +56,8 @@ export default class SliderLayout extends Component {
                                                 </div>
                                                 <div className="relative mt-6 flex-1 px-4 sm:px-6">
                                                     {/* Replace with your content */}
-                                                    <div className="flex justify-between">
-                                                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                                                            Email
-                                                        </label>
-                                                        <span className="text-sm text-gray-500" id="email-optional">
-                                                            Optional
-                                                        </span>
-                                                    </div>
-                                                    <div className="mt-1">
-                                                        <input
-                                                            type="email"
-                                                            name="email"
-                                                            id="email"
-                                                            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                                                            placeholder="you@example.com"
-                                                            aria-describedby="email-optional"
-                                                        />
+                                                    <div>
+                                                    {this.props.children}
                                                     </div>
                                                 </div>
                                             </div>
