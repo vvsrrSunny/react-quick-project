38a1ea7de5d14aaccf2dcf5b361dec02b39fdb44
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-08-10 02:05:55 +0800

    added working code

--- src/components/UserEdit.jsx
+++ src/components/UserEdit.jsx
@@ -21,7 +21,7 @@ const UserEdit = (props) => {
                         id="email"
                         className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
                         placeholder="you@example.com"
-                        defaultValue="John"
+                        defaultValue={props.person.email}
                         onChange={setTxtLetter}
                         aria-describedby="email-optional"
                     />
