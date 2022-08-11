4072812a2d9049f1a15a50e42832d38786fac994
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-08-12 02:23:20 +0800

    proper update is successfull

--- src/components/UserEdit.jsx
+++ src/components/UserEdit.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import LabelAndInput from "./LabelAndInput";
 const UserEdit = (props) => {
-    const [editedPerson, setEditedPerson] = useState({...props.person});
+    const [editPerson, setEditPerson] = useState({...props.person});
 
     const updatedPerson = (person) => {
-        setEditedPerson(person);
+        setEditPerson(person);
       }
     return (
         <div className="flex flex-col space-y-4 divide-y-2">
-            <LabelAndInput editCallback={updatedPerson} personValueKey='name' value={props.person.name} person={props.person}/>
-            <LabelAndInput editCallback={updatedPerson} personValueKey='title' value={props.person.title} person={props.person}/>
-            <LabelAndInput editCallback={updatedPerson} personValueKey='email' value={props.person.email} person={props.person}/>
-            <LabelAndInput editCallback={updatedPerson} personValueKey='role' value={props.person.role} person={props.person}/>
+            <LabelAndInput editCallback={updatedPerson} personValueKey='name' value={props.person.name} person={editPerson}/>
+            <LabelAndInput editCallback={updatedPerson} personValueKey='title' value={props.person.title} person={editPerson}/>
+            <LabelAndInput editCallback={updatedPerson} personValueKey='email' value={props.person.email} person={editPerson}/>
+            <LabelAndInput editCallback={updatedPerson} personValueKey='role' value={props.person.role} person={editPerson}/>
         </div>
     )
 };
