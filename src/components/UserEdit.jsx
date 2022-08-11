6d144f0dd125da3093407fff056ed79c60801a55
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-08-12 00:50:58 +0800

    added edit

--- src/components/UserEdit.jsx
+++ src/components/UserEdit.jsx
@@ -6,27 +6,11 @@ const UserEdit = (props) => {
     }
     return (
 
-        <div className="flex flex-col  space-y-4 divide-y-2">
+        <div className="flex flex-col space-y-4 divide-y-2">
             <LabelAndInput labelName='Name' value={props.person.name}/>
-            <div>
-                <div className="flex justify-between mt-2">
-                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                        Title
-                    </label>
-                </div>
-                <div className="mt-1">
-                    <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                        placeholder="you@example.com"
-                        defaultValue={props.person.email}
-                        onChange={setTxtLetter}
-                        aria-describedby="email-optional"
-                    />
-                </div>
-            </div>
+            <LabelAndInput labelName='Title' value={props.person.title}/>
+            <LabelAndInput labelName='Email' value={props.person.email}/>
+            <LabelAndInput labelName='Role' value={props.person.role}/>
         </div>
     )
 };
