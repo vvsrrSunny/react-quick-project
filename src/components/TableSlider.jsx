0ab8d1c806ee3d4b883ee11457d0ecfcd7e24f50
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-08-14 00:56:34 +0800

    edit freature in full swing

--- src/components/TableSlider.jsx
+++ src/components/TableSlider.jsx
@@ -3,13 +3,17 @@ import SliderLayout from "./SliderLayout";
 import UserEdit from "./UserEdit";
 const TableSlider = React.forwardRef((props, sliderLayoutRef) => {
 
-    const [editPerson, setEditPerson] = useState({...props.person});
+    const [editPerson, setEditPerson] = useState(props.person);
     const updatedPerson = (person) => {
         setEditPerson(person);
-      }
+    }
+
+    const savePerson = () => {
+        props.updatePeople(editPerson);
+    }
     return (
-        <SliderLayout ref={sliderLayoutRef}>
-            <UserEdit setEditPerson = {updatedPerson} person = {props.person}/>
+        <SliderLayout ref={sliderLayoutRef} save={savePerson}>
+            <UserEdit setEditPerson={updatedPerson} person={props.person} />
         </SliderLayout>
     )
 });
